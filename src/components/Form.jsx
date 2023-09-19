3304be5f500dabcfa70c395baf83ca12362a7fbf
Veronica Rojas <ar_veronica@yahoo.com>
2023-09-19 20:11:49 +0100

    replace .then block with async

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,9 +10,18 @@ function Form() {
   const [allMemes, setAllMemes] = useState([]);
 
 useEffect(() => {
-fetch("https://api.imgflip.com/get_memes")
-  .then(res => res.json())
-  .then(data => setAllMemes(data.data.memes))
+
+  async function getMemes() {
+    const response = await fetch("https://api.imgflip.com/get_memes");
+    const data = await response.json()
+    setAllMemes(data.data.memes);
+  }
+
+  getMemes();
+
+// fetch("https://api.imgflip.com/get_memes")
+//   .then(res => res.json())
+//   .then(data => setAllMemes(data.data.memes))
 }, []);
 
 function randomMeme() {
